68d6b38f9dc8d84e5cffd91f23903ba230f7fb68
Arash <arashfarahani@hotmail.com>
2019-06-10 23:23:00 +0430

    Create new module for test multiple container

---
+++ person/src/main/java/com/sarv/controller/PersonController.java
@@ -0,0 +1,14 @@
+package com.sarv.controller;
+
+import com.sarv.model.Person;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+    @RequestMapping("/person")
+    public Person greeting(@RequestParam(value="name", defaultValue="Arash") String name) {
+        return new Person(name, "Farahani");
+    }
+}
